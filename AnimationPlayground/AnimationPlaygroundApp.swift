fdbb4e0fc44fc2420265b8844e594bf18bcecd13
Rei Soemanto <reresoemanto@gmail.com>
2026-04-16 17:11:42 +0700

    Initial Commit

---
+++ AnimationPlayground/AnimationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPlaygroundApp.swift
+//  AnimationPlayground
+//
+//  Created by Rei Soemanto on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
